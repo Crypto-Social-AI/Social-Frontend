b2fc3217f5649d5cb13d92b7e7e0b2703da2bbc6
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-04 20:07:15 +0200

    rename header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import { ConnectWallet } from '@thirdweb-dev/react';
 import appLogo from '../../assets/images/super-saiyan-bot-logo.png';
+import Navigation from './Navigation';
 
-export default function Navbar() {
+export default function Header() {
   return (
-    <header className='py-4 px-8 border-b border-b-orange-600'>
-      <div className='flex justify-between items-center'>
+    <header className='flex flex-col'>
+      <div className='flex justify-between items-center border-b border-b-slate-800 py-4 px-8'>
         <div className='w-20 h-20'>
           <img className='rounded-full' src={appLogo} alt='Super Saiyan Bot' />
         </div>
         <ConnectWallet />
       </div>
+      <Navigation />
     </header>
   );
 }
