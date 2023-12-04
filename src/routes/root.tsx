d2fd320a243d8b1b3f87abb5bdf8d2bba8dd8b53
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-04 16:48:07 +0200

    create root component

---
+++ src/routes/root.tsx
@@ -0,0 +1,17 @@
+import { ConnectWallet } from '@thirdweb-dev/react';
+import { FaWallet } from 'react-icons/fa6';
+
+export default function Root() {
+  return (
+    <div className='flex flex-col gap-6 justify-center items-center mt-12 mx-8 border border-yellow-400 rounded-xl py-6 px-4 bg-gradient-to-tr from-yellow-400 via-black to-black'>
+      <div className='rounded-full w-16 h-16 bg-yellow-600 flex justify-center items-center'>
+        <FaWallet size={24} />
+      </div>
+      <h2 className='text-3xl text-contrast'>Connect wallet</h2>
+      <span className='text-text-primary'>Connect your wallet by clicking the button below to proceed</span>
+      <div>
+        <ConnectWallet />
+      </div>
+    </div>
+  );
+}
