317b099c7a64577f68ea1f24b969ffff8d05e359
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-04 20:07:58 +0200

    add request fn to fetch social calls

---
+++ src/lib/utils/requests/getSocialCalls.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+import { type SocialPosts, PostSchema } from '../../types';
+
+export async function getSocialCalls(): Promise<SocialPosts> {
+  const url = 'http://localhost:4000/socialPost';
+
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error('Failed to fetch');
+    }
+
+    const PostsArraySchema = z.array(PostSchema);
+    const data = await res.json();
+    const socialPosts = data.socialPosts;
+    const validatedSocialPosts = PostsArraySchema.safeParse(socialPosts);
+
+    if (!validatedSocialPosts.success) {
+      console.error(validatedSocialPosts.error);
+      return [];
+    }
+
+    return validatedSocialPosts.data;
+  } catch (error) {
+    console.error('getSocialCalls', { error });
+    return [];
+  }
+}
