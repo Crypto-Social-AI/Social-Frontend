6129cd7931b6b9143dd7a13cd665687aaa51501d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-05 19:39:17 +0200

    add home link to app logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import { ConnectWallet } from '@thirdweb-dev/react';
 import appLogo from '../../assets/images/super-saiyan-bot-logo.png';
 import Navigation from './Navigation';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className='flex flex-col'>
       <div className='flex justify-between items-center border-b border-b-slate-800 py-4 px-8'>
-        <div className='w-20 h-20'>
-          <img className='rounded-full' src={appLogo} alt='Super Saiyan Bot' />
-        </div>
+        <Link to='/'>
+          <div className='w-20 h-20'>
+            <img className='rounded-full' src={appLogo} alt='Super Saiyan Bot' />
+          </div>
+        </Link>
         <ConnectWallet />
       </div>
       <Navigation />
