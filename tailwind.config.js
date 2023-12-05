de7e6673efee860afb4664ac4fcb556e5362fbc1
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-05 15:33:47 +0200

    add css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        contrast: 'var(--contrast)',
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'text-contrast': 'var(--text-contrast)',
+        blue: 'var(--text-blue)',
+        'blue-hover': 'var(--text-blue-hover)',
+        hamburger: 'var(--hamburger-color)',
+        'green-primary': 'var(--green-primary)',
+        'red-primary': 'var(--red-primary)',
+        'etherscan-logo-top': 'var(--etherscan-logo-top)',
+        'etherscan-logo-bottom': 'var(--etherscan-logo-bottom)',
+      },
+    },
   },
   plugins: [],
 };
