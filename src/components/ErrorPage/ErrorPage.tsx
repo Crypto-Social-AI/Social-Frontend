4869dc19bf2c1c03930a9e0ece42ed22fc3c3182
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-05 09:28:37 +0200

    update error boundary, no need for custom types

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,29 +1,24 @@
-import { Link, useRouteError } from 'react-router-dom';
-
-type Error = {
-  message: string;
-  stack: string;
-};
-
-type RouteError = {
-  status: number;
-  statusText?: string;
-  internal: boolean;
-  data: string;
-  error: Error;
-};
+import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
-  const error = useRouteError() as RouteError;
+  const error = useRouteError() as Error;
   console.error(error);
 
   return (
     <div className='flex flex-col items-center justify-center gap-4 h-screen text-4xl'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText ?? error.error.message}</i>
-      </p>
+      {isRouteErrorResponse(error) ? (
+        <p>
+          <i>
+            {error.status} {error.statusText}
+          </i>
+        </p>
+      ) : (
+        <p>
+          <i>{error.message ?? error}</i>
+        </p>
+      )}
       <Link
         className='bg-yellow-400 rounded-xl text-slate-900 px-4 py-2 hover:bg-yellow-200 border border-yellow-700 transition-colors duration-150'
         to='/'
