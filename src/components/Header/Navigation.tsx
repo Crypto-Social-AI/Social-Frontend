bfa1a01a07232d588713c819392739d0b7e3bb6b
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-06 19:33:38 +0200

    hide navigation if wallet is not connected

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useAddress } from '@thirdweb-dev/react';
 import { NavLink } from 'react-router-dom';
 
 type NavLinkProps = {
@@ -5,40 +6,40 @@ type NavLinkProps = {
 };
 
 export default function Navigation() {
+  const address = useAddress();
+
   return (
-    <nav className='border-b border-b-slate-800 py-8'>
-      <ul className='flex gap-12 justify-center items-center'>
-        <li>
-          <NavLink
-            to='/'
-            className={({ isActive }: NavLinkProps) =>
-              isActive ? 'text-yellow-300 underline' : 'underline hover:text-yellow-400 transition-colors duration-150'
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/socialCalls'
-            className={({ isActive }: NavLinkProps) =>
-              isActive ? 'text-yellow-300 underline' : 'underline hover:text-yellow-400 transition-colors duration-150'
-            }
-          >
-            Telegram calls
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/socialAccounts'
-            className={({ isActive }: NavLinkProps) =>
-              isActive ? 'text-yellow-300 underline' : 'underline hover:text-yellow-400 transition-colors duration-150'
-            }
-          >
-            Top accounts
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {address ? (
+        <nav className='border-b border-b-slate-800 py-8'>
+          <ul className='flex gap-12 justify-center items-center'>
+            <li>
+              <NavLink
+                to='/socialCalls'
+                className={({ isActive }: NavLinkProps) =>
+                  isActive
+                    ? 'text-yellow-300 underline'
+                    : 'underline hover:text-yellow-400 transition-colors duration-150'
+                }
+              >
+                Telegram calls
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/socialAccounts'
+                className={({ isActive }: NavLinkProps) =>
+                  isActive
+                    ? 'text-yellow-300 underline'
+                    : 'underline hover:text-yellow-400 transition-colors duration-150'
+                }
+              >
+                Top accounts
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : null}
+    </>
   );
 }
