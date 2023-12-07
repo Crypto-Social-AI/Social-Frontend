95d80bf84f8fae5272dad68e3f9351e83056207d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-07 21:17:11 +0200

    extract router into own file and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root';
-import SocialCalls, { loader as socialCallsLoader } from './routes/socialCalls';
-import SocialAccounts, { loader as socialAccountsLoader } from 'routes/socialAccounts';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import Layout from './components/Layout/Layout';
-
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/',
-        element: <Root />,
-      },
-      {
-        path: '/socialCalls',
-        element: <SocialCalls />,
-        loader: socialCallsLoader,
-      },
-      {
-        path: '/socialAccounts',
-        element: <SocialAccounts />,
-        loader: socialAccountsLoader,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from 'react-router-dom';
+import { router } from 'router/router';
 
 function App() {
   return <RouterProvider router={router} fallbackElement={<span>Loading...</span>} />;
---
+++ src/router/router.tsx
@@ -0,0 +1,20 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Root from './routes/root';
+import PrivateRouteWrapper from './routes/privateRoute';
+import SocialCalls, { loader as socialCallsLoader } from './routes/socialCalls';
+import SocialAccounts from './routes/socialAccounts';
+import Layout from 'components/Layout/Layout';
+import ErrorPage from 'components/ErrorPage/ErrorPage';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path='/' element={<Root />} />
+      <Route element={<PrivateRouteWrapper />}>
+        <Route path='socialCalls' element={<SocialCalls />} loader={socialCallsLoader} />
+        <Route path='socialAccounts' element={<SocialAccounts />} />
+      </Route>
+      <Route path='*' element={<ErrorPage />} />
+    </Route>,
+  ),
+);
