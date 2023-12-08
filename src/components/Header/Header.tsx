73748d3b71e92f6665ba897b87f3bb2f09ecd967
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-12-08 23:39:24 +0200

    remove uneeded navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAddress } from '@thirdweb-dev/react';
 import ConnectWalletBox from 'components/ConnectWalletBox/ConnectWalletBox';
 import Navigation from './Navigation';
 
 export default function Header() {
-  const address = useAddress();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (address) {
-      navigate('/socialCalls');
-    }
-  }, [address]);
-
   return (
     <header className='flex flex-col'>
       <ConnectWalletBox />
