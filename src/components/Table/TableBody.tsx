7bb5d346158113adfd63d4d0798e8a740d1e1cfc
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-02 21:42:21 +0000

    add loading skeleton to table body

--- src/components/Table/TableBody.tsx
+++ src/components/Table/TableBody.tsx
@@ -1,16 +1,32 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { type CommonTableProps } from '../../lib/types';
 import TableRow from './TableRow';
 
 type TableBodyProps = CommonTableProps & {
   displayedData: any;
+  loading?: boolean;
 };
 
-function TableBody({ displayedData, ...props }: TableBodyProps) {
+function TableBody({ displayedData, loading, ...props }: TableBodyProps) {
+  const skeletonRowCount = 10;
+  const columnCount = props?.renderedRecords?.length;
+
+  const renderSkeletonRows = () => {
+    return Array.from({ length: skeletonRowCount }).map((_, rowIndex) => (
+      <tr key={`skeleton-row-${rowIndex}`}>
+        <td colSpan={columnCount + 1}>
+          <Skeleton height={40} />
+        </td>
+      </tr>
+    ));
+  };
+
   return (
     <tbody className='table-rounded-bottom'>
-      {displayedData.map((item: any) => (
-        <TableRow key={item?.[props.idProp]} record={item} {...props} />
-      ))}
+      {loading
+        ? renderSkeletonRows()
+        : displayedData?.map((item: any) => <TableRow key={item?.[props.idProp]} record={item} {...props} />)}
     </tbody>
   );
 }
