b8e1670dba50554e5467b94cbc09e271718c7318
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-02 21:42:56 +0000

    wrap app in global skeleton theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { SkeletonTheme } from 'react-loading-skeleton';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain='ethereum' clientId={process.env.THIRDWEB_CLIENT_ID}>
-      <App />
-    </ThirdwebProvider>
+    <SkeletonTheme baseColor='#222' highlightColor='#333'>
+      <ThirdwebProvider activeChain='ethereum' clientId={process.env.THIRDWEB_CLIENT_ID}>
+        <App />
+      </ThirdwebProvider>
+    </SkeletonTheme>
   </React.StrictMode>,
 );
