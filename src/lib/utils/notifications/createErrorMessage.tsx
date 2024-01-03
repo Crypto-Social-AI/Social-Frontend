77f127c3d2af0db1edd2e4ad5fcf66b9241f9f24
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-03 16:55:08 +0000

    add function to create error message toast

---
+++ src/lib/utils/notifications/createErrorMessage.tsx
@@ -0,0 +1,17 @@
+import { toast } from 'react-toastify';
+
+const createErrorMessage = (text: string) => {
+  toast.error(text, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    toastId: 'custom-id-yes',
+    className: 'bg-secondary text-slate-200',
+    theme: 'dark',
+  });
+};
+export default createErrorMessage;
