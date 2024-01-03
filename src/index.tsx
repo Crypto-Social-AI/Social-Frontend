687e04ff185cac05f6ad5f75a9c60c545c2fd070
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-03 16:52:21 +0000

    add toast container to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import { SkeletonTheme } from 'react-loading-skeleton';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <SkeletonTheme baseColor='#222' highlightColor='#333'>
       <ThirdwebProvider activeChain='ethereum' clientId={process.env.THIRDWEB_CLIENT_ID}>
         <App />
