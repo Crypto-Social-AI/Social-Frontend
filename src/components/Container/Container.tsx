31febceef6e4188d3d61c94f78115d351ff4e990
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-03 17:09:08 +0000

    increase width of tables

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,5 +4,5 @@ type ContainerProps = {
 };
 
 export default function Container({ children, className }: ContainerProps) {
-  return <div className={`max-w-[130rem] mx-auto my-12 ${className}`}>{children}</div>;
+  return <div className={`max-w-[180rem] mx-auto my-12 ${className}`}>{children}</div>;
 }
