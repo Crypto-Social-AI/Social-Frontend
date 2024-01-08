0aeff080ae4f021dce5ecc13fbd2c3a96717e720
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-08 12:59:45 +0000

    render pagination controls + add state to support pagination

--- src/router/routes/socialCalls.tsx
+++ src/router/routes/socialCalls.tsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react';
 import { useAddress } from '@thirdweb-dev/react';
-import { getSocialCalls } from 'lib/utils/requests/getSocialCalls';
+import { type SocialCallsResponse, getSocialCalls } from 'lib/utils/requests/getSocialCalls';
 import { type SocialPosts } from 'lib/types';
 import SocialCallsTable from 'components/SocialCallsTable/SocialCallsTable';
+import PaginationControls from 'components/PaginationControls/PaginationControls';
 
 export default function SocialCalls() {
   const address = useAddress();
   const [socialCalls, setSocialCalls] = useState<SocialPosts | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
   useEffect(() => {
     if (address) {
       setLoading(true);
-      getSocialCalls()
-        .then((data) => setSocialCalls(data))
+      getSocialCalls(currentPage)
+        .then((data: SocialCallsResponse) => {
+          setSocialCalls(data.socialPosts);
+          setTotalPages(data.totalPages);
+        })
         .catch((error) => console.error({ error }))
         .finally(() => setLoading(false));
     }
-  }, [address]);
+  }, [address, currentPage]);
 
-  return <SocialCallsTable socialCalls={socialCalls} loading={loading} />;
+  return (
+    <>
+      <SocialCallsTable socialCalls={socialCalls} loading={loading} />
+      <PaginationControls currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+    </>
+  );
 }
