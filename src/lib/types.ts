55e10b21ef4e3926daa2782ec1b67a8fad2736f5
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-08 10:41:07 +0000

    update social posts types and zod schemas

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,19 +1,26 @@
 import { z } from 'zod';
 
+export const PaginationInfoSchema = z.object({
+  page: z.number(),
+  limit: z.number(),
+});
+
 export const PostSchema = z.object({
-  id: z.number(),
-  message: z.string().nullable(),
-  messageUnixTimestampInSeconds: z.number(),
+  channelLink: z.string().optional().nullable(),
+  channelName: z.string().optional().nullable(),
   codeUnixTimestampInSeconds: z.number(),
-  tokenName: z.string().nullable(),
-  tokenSymbol: z.string().nullable(),
-  tokenImage: z.string().nullable(),
-  pairAddress: z.string().optional(),
-  tokenAddress: z.string().optional(),
+  id: z.number(),
+  isDiscord: z.boolean().default(false),
+  isTelegram: z.boolean().default(false),
+  isTwitter: z.boolean().default(false),
   isUniswapV2: z.boolean().default(false),
   isUniswapV3: z.boolean().default(false),
-  marketcapAtMessage: z.string().nullable(),
   liquidityInCommonTokenAtMessage: z.string().nullable(),
+  marketcapAtMessage: z.string().nullable(),
+  message: z.string().nullable(),
+  messageUnixTimestampInSeconds: z.number(),
+  pairAddress: z.string().optional(),
+  photo: z.string().optional().nullable(),
   priceInUsdAtMessageAtMessage: z.string().nullable(),
   priceInComommonTokenAtMessage: z.string().nullable(),
   priceInComommonTokenOneMinuteLater: z.string().nullable(),
@@ -31,17 +38,28 @@ export const PostSchema = z.object({
   priceInComommonTokenThreeWeeksLater: z.string().nullable(),
   priceInComommonTokenOneMonthLater: z.string().nullable(),
   socialAccountId: z.number(),
+  tokenAddress: z.string().optional(),
+  tokenImage: z.string().nullable(),
+  tokenName: z.string().nullable(),
+  tokenSymbol: z.string().nullable(),
   username: z.string().optional(),
-  channelLink: z.string().optional().nullable(),
-  channelName: z.string().optional().nullable(),
-  photo: z.string().optional().nullable(),
-  isTwitter: z.boolean().default(false),
-  isTelegram: z.boolean().default(false),
-  isDiscord: z.boolean().default(false),
+});
+
+export const SocialPostsSchema = z.object({
+  next: PaginationInfoSchema,
+  results: z.array(PostSchema),
+});
+
+export const PostsResponseSchema = z.object({
+  message: z.string(),
+  totalPages: z.number(),
+  currentPage: z.number(),
+  socialPosts: SocialPostsSchema,
 });
 
 export type SocialPost = z.infer<typeof PostSchema>;
 export type SocialPosts = SocialPost[];
+export type SocialPostsResponse = z.infer<typeof PostsResponseSchema>;
 
 export type BgColor = 'base' | 'secondary' | 'contrast';
 
