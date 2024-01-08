d236138884fbce395703a6469e3a0c5ae20a11ea
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-08 18:07:01 +0000

    style pagination controls a little better

--- src/components/PaginationControls/PaginationControls.tsx
+++ src/components/PaginationControls/PaginationControls.tsx
@@ -1,3 +1,5 @@
+import { HiChevronRight, HiChevronLeft } from 'react-icons/hi';
+
 type PaginationControlsProps = {
   currentPage: number;
   totalPages: number;
@@ -6,15 +8,23 @@ type PaginationControlsProps = {
 
 export default function PaginationControls({ currentPage, totalPages, onPageChange }: PaginationControlsProps) {
   return (
-    <div>
-      <button disabled={currentPage <= 1} onClick={() => onPageChange(currentPage - 1)}>
-        Previous
+    <div className='my-8 flex items-center gap-1 border border-primary rounded-xl w-fit'>
+      <button
+        className='flex items-center border-r border-primary hover:bg-secondary transition-all duration-150 rounded-l-xl'
+        disabled={currentPage <= 1}
+        onClick={() => onPageChange(currentPage - 1)}
+      >
+        <HiChevronLeft size={24} />
       </button>
-      <span>
-        Page {currentPage} of {totalPages}
+      <span className='text-2xl px-4'>
+        {currentPage} of {totalPages}
       </span>
-      <button disabled={currentPage >= totalPages} onClick={() => onPageChange(currentPage + 1)}>
-        Next
+      <button
+        className='flex items-center border-l border-primary hover:bg-secondary transition-all duration-150 rounded-r-xl'
+        disabled={currentPage >= totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+      >
+        <HiChevronRight size={24} />
       </button>
     </div>
   );
