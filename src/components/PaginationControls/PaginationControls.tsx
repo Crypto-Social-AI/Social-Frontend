11182fcb0f34f0c6560337765301529df85539d4
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-09 18:13:53 +0000

    Add checkbox for custom limit UI in pagination controls

--- src/components/PaginationControls/PaginationControls.tsx
+++ src/components/PaginationControls/PaginationControls.tsx
@@ -1,31 +1,62 @@
 import { HiChevronRight, HiChevronLeft } from 'react-icons/hi';
+import { HiChevronDown } from 'react-icons/hi2';
 
 type PaginationControlsProps = {
   currentPage: number;
   totalPages: number;
+  showItemsPerPage?: boolean;
   onPageChange: (currentPage: number) => void;
+  onItemsPerPageChange: (newLimit: number) => void;
 };
 
-export default function PaginationControls({ currentPage, totalPages, onPageChange }: PaginationControlsProps) {
+export default function PaginationControls({
+  currentPage,
+  totalPages,
+  onPageChange,
+  onItemsPerPageChange,
+  showItemsPerPage = true,
+}: PaginationControlsProps) {
   return (
-    <div className='my-8 flex items-center gap-1 border border-primary rounded-xl w-fit'>
-      <button
-        className='flex items-center border-r border-primary hover:bg-secondary transition-all duration-150 rounded-l-xl'
-        disabled={currentPage <= 1}
-        onClick={() => onPageChange(currentPage - 1)}
-      >
-        <HiChevronLeft size={24} />
-      </button>
-      <span className='text-2xl px-4'>
-        {currentPage} of {totalPages}
-      </span>
-      <button
-        className='flex items-center border-l border-primary hover:bg-secondary transition-all duration-150 rounded-r-xl'
-        disabled={currentPage >= totalPages}
-        onClick={() => onPageChange(currentPage + 1)}
-      >
-        <HiChevronRight size={24} />
-      </button>
+    <div className='flex justify-between items-center'>
+      <div className='my-8 flex items-center gap-1 border border-primary rounded-xl w-fit'>
+        <button
+          className='flex items-center border-r border-primary hover:bg-secondary transition-all duration-150 rounded-l-xl'
+          disabled={currentPage <= 1}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          <HiChevronLeft size={24} />
+        </button>
+        <span className='text-2xl px-4'>
+          {currentPage} of {totalPages}
+        </span>
+        <button
+          className='flex items-center border-l border-primary hover:bg-secondary transition-all duration-150 rounded-r-xl'
+          disabled={currentPage >= totalPages}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          <HiChevronRight size={24} />
+        </button>
+      </div>
+      {showItemsPerPage ? (
+        <div className='flex items-center gap-3'>
+          <span>Items per page</span>
+          <div className='relative inline-block'>
+            <select
+              className='block appearance-none w-full bg-black text-white py-2 pl-4 pr-6 rounded leading-tight focus:outline-none focus:border-blue-500 border border-gray-400'
+              name='limitPerPage'
+              id='limitPerPage'
+              onChange={(e) => onItemsPerPageChange(Number(e.target.value))}
+            >
+              <option value='20'>20</option>
+              <option value='50'>50</option>
+              <option value='100'>100</option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2'>
+              <HiChevronDown />
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
