ec15afd38a73f4bf9c581d790c7da003740f4462
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-09 18:13:20 +0000

    implement custom page limit UI

--- src/router/routes/socialCalls.tsx
+++ src/router/routes/socialCalls.tsx
@@ -4,35 +4,57 @@ import { type SocialCallsResponse, getSocialCalls } from 'lib/utils/requests/get
 import { type SocialPosts } from 'lib/types';
 import SocialCallsTable from 'components/SocialCallsTable/SocialCallsTable';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
+import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
 
 export default function SocialCalls() {
   const address = useAddress();
   const [socialCalls, setSocialCalls] = useState<SocialPosts | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
   const [loading, setLoading] = useState(false);
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
   };
 
-  useEffect(() => {
+  const handleItemsPerPageChange = (newLimit: number) => {
+    setItemsPerPage(newLimit);
+    setCurrentPage(1);
+  };
+
+  const fetchData = async (page: number, limit: number) => {
     if (address) {
       setLoading(true);
-      getSocialCalls(currentPage)
-        .then((data: SocialCallsResponse) => {
-          setSocialCalls(data.socialPosts);
-          setTotalPages(data.totalPages);
-        })
-        .catch((error) => console.error({ error }))
-        .finally(() => setLoading(false));
+      try {
+        const data: SocialCallsResponse = await getSocialCalls(page, limit);
+        setSocialCalls(data.socialPosts);
+        setTotalPages(data.totalPages);
+      } catch (error) {
+        console.error({ error });
+      } finally {
+        setLoading(false);
+      }
     }
-  }, [address, currentPage]);
+  };
+
+  useEffect(() => {
+    async function fetchDataAsync() {
+      await fetchData(currentPage, itemsPerPage);
+    }
+
+    fetchDataAsync().catch(console.error);
+  }, [address, currentPage, itemsPerPage]);
 
   return (
     <>
       <SocialCallsTable socialCalls={socialCalls} loading={loading} />
-      <PaginationControls currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+      <PaginationControls
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+        onItemsPerPageChange={handleItemsPerPageChange}
+      />
     </>
   );
 }
