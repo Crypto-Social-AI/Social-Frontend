137ace513f2b032012d975070d9971228e48c6d1
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-09 18:14:30 +0000

    implement custom limit functionality

--- src/router/routes/socialAccounts.tsx
+++ src/router/routes/socialAccounts.tsx
@@ -5,40 +5,57 @@ import SocialAccountsTable from 'components/SocialAccountsTable/SocialAccountsTa
 import { type SocialAccountWithPost } from 'lib/types';
 import { getSocialAccounts } from 'lib/utils/requests/getSocialAccounts';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
+import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
 
 export default function SocialAccounts() {
   const address = useAddress();
   const [socialAccounts, setSocialAccounts] = useState<SocialAccountWithPost[] | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (address) {
-      setLoading(true);
-      // Update the getSocialAccounts call to include the current page
-      getSocialAccounts(currentPage)
-        .then((response) => {
-          setSocialAccounts(response.socialAccounts);
-          setTotalPages(response.totalPages); // Assuming response includes totalPages
-        })
-        .catch((error) => {
-          console.error('Failed to fetch social accounts:', error);
-        })
-        .finally(() => setLoading(false));
-    }
-  }, [address, currentPage]);
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
   };
 
+  const handleItemsPerPageChange = (newLimit: number) => {
+    setItemsPerPage(newLimit);
+    setCurrentPage(1);
+  };
+
+  const fetchData = async (page: number, limit: number) => {
+    if (address) {
+      setLoading(true);
+      try {
+        const data = await getSocialAccounts(page, limit);
+        setSocialAccounts(data.socialAccounts);
+        setTotalPages(data.totalPages);
+      } catch (error) {
+        console.error({ error });
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    async function fetchDataAsync() {
+      await fetchData(currentPage, itemsPerPage);
+    }
+
+    fetchDataAsync().catch(console.error);
+  }, [address, currentPage, itemsPerPage]);
+
   return (
     <Container className='px-12'>
       <SocialAccountsTable socialAccounts={socialAccounts} loading={loading} />
-      {totalPages > 1 && (
-        <PaginationControls currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
-      )}
+      <PaginationControls
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+        onItemsPerPageChange={handleItemsPerPageChange}
+      />
     </Container>
   );
 }
