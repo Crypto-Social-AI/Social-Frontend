10a09c2c913d3abfac575a3e8294195eb1cf5d9e
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-10 22:00:58 +0000

    add functionality to sort table cols by key

--- src/router/routes/socialAccounts.tsx
+++ src/router/routes/socialAccounts.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useAddress } from '@thirdweb-dev/react';
 import Container from 'components/Container/Container';
 import SocialAccountsTable from 'components/SocialAccountsTable/SocialAccountsTable';
-import { type SocialAccountWithPost } from 'lib/types';
+import { type SortConfig, type SocialAccountWithPost } from 'lib/types';
 import { getSocialAccounts } from 'lib/utils/requests/getSocialAccounts';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
 import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
@@ -13,8 +13,36 @@ export default function SocialAccounts() {
   const [socialAccounts, setSocialAccounts] = useState<SocialAccountWithPost[] | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null);
   const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
   const [loading, setLoading] = useState(false);
+
+  const handleSortChange = (key: string) => {
+    setSortConfig((currentSortConfig): SortConfig | null => {
+      if (currentSortConfig && currentSortConfig.key === key) {
+        return {
+          key,
+          direction: currentSortConfig.key === 'asc' ? 'desc' : 'asc',
+        };
+      }
+      return { key, direction: 'asc' };
+    });
+  };
+
+  const sortData = (data: SocialAccountWithPost[] | null) => {
+    if (!sortConfig) return data;
+    if (!data) return null;
+
+    return [...data].sort((a, b) => {
+      const keyA = a[sortConfig.key as keyof SocialAccountWithPost];
+      const keyB = b[sortConfig.key as keyof SocialAccountWithPost];
+      if (keyA < keyB) return sortConfig.direction === 'asc' ? -1 : 1;
+      if (keyA > keyB) return sortConfig.direction === 'asc' ? 1 : -1;
+      return 0;
+    });
+  };
+
+  const sortedData = sortData(socialAccounts);
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
@@ -50,7 +78,12 @@ export default function SocialAccounts() {
 
   return (
     <Container className='px-12'>
-      <SocialAccountsTable socialAccounts={socialAccounts} loading={loading} />
+      <SocialAccountsTable
+        socialAccounts={sortedData}
+        loading={loading}
+        sortConfig={sortConfig}
+        onHandleSortChange={handleSortChange}
+      />
       <PaginationControls
         currentPage={currentPage}
         totalPages={totalPages}
