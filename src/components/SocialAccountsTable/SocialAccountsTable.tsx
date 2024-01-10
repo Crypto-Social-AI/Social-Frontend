9eb05a5dcde4b89ee0294a27af9025e9b5bf2100
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-10 22:01:21 +0000

    pass sorting props to child table

--- src/components/SocialAccountsTable/SocialAccountsTable.tsx
+++ src/components/SocialAccountsTable/SocialAccountsTable.tsx
@@ -1,14 +1,21 @@
 import Table from 'components/Table/Table';
-import { type SocialAccountWithPost } from 'lib/types';
+import { type SortConfig, type SocialAccountWithPost } from 'lib/types';
 import socialAccountsData from './socialAccountsData';
 import ExpandedContent from './ExpandedContent';
 
 type SocialAccountsTableProps = {
   socialAccounts: SocialAccountWithPost[] | null;
   loading: boolean;
+  sortConfig: SortConfig | null;
+  onHandleSortChange: (key: string) => void;
 };
 
-export default function SocialAccountsTable({ socialAccounts, loading }: SocialAccountsTableProps) {
+export default function SocialAccountsTable({
+  socialAccounts,
+  loading,
+  sortConfig,
+  onHandleSortChange,
+}: SocialAccountsTableProps) {
   return (
     <Table
       className='h-error-or-empty-table-height'
@@ -16,6 +23,8 @@ export default function SocialAccountsTable({ socialAccounts, loading }: SocialA
       dataSrc={socialAccounts}
       displayedData={socialAccounts}
       renderedRecords={socialAccountsData}
+      sortConfig={sortConfig}
+      handleSortClick={onHandleSortChange}
       idProp='id'
       renderExpandedContent={(record) => <ExpandedContent posts={record.posts} />}
       isExpandable
