90682c1b0d7fae3c26f0fee9de530c284cb9811d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-11 21:22:55 +0000

    add all data needed to sort by prices inside the component

--- src/router/routes/socialCalls.tsx
+++ src/router/routes/socialCalls.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState } from 'react';
 import { useAddress } from '@thirdweb-dev/react';
 import { type SocialCallsResponse, getSocialCalls } from 'lib/utils/requests/getSocialCalls';
-import { type SortConfig, type SocialPosts } from 'lib/types';
+import { type SortConfig, type SocialPosts, type SocialPost } from 'lib/types';
 import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
 import sortData from 'lib/utils/helpers/sorting/sorting';
 import SocialCallsTable from 'components/SocialCallsTable/SocialCallsTable';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
+import {
+  getCurrentProfitPercentage,
+  getMaxProfitPercentage,
+  getProfitForTimeRange,
+} from 'lib/utils/helpers/calculations/calculateProfit';
+
+type ExtendedSocialPost = SocialPost & {
+  currentProfitLoss?: number | null;
+  maxProfitLoss?: number | null;
+  profitForOneHour?: number | null;
+  profitForEightHours?: number | null;
+  profitForOneDayHour?: number | null;
+  profitForSevenDays?: number | null;
+};
 
 // TODO: Add custom hook or HOC to keep code DRY (SocialCalls and SocialAccounts virtually the same code)
 export default function SocialCalls() {
@@ -13,12 +27,12 @@ export default function SocialCalls() {
   const [socialCalls, setSocialCalls] = useState<SocialPosts | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null);
+  const [sortConfig, setSortConfig] = useState<SortConfig<keyof SocialPost> | null>(null);
   const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
   const [loading, setLoading] = useState(false);
 
-  const handleSortChange = (key: string) => {
-    setSortConfig((currentSortConfig): SortConfig | null => {
+  const handleSortChange = (key: keyof SocialPost) => {
+    setSortConfig((currentSortConfig): SortConfig<keyof SocialPost> | null => {
       if (currentSortConfig && currentSortConfig.key === key) {
         return {
           key,
@@ -29,7 +43,20 @@ export default function SocialCalls() {
     });
   };
 
-  const sortedData = sortData(socialCalls, sortConfig);
+  // consider wrapping in useMemo
+  const processData = (data: SocialPost[]): ExtendedSocialPost[] =>
+    data?.map((post) => ({
+      ...post,
+      currentProfitLoss: getCurrentProfitPercentage(post),
+      maxProfitLoss: getMaxProfitPercentage(post),
+      profitForOneHour: getProfitForTimeRange(post, 'priceInComommonTokenOneHourLater'),
+      profitForEightHours: getProfitForTimeRange(post, 'priceInComommonTokenEightHoursLater'),
+      profitForOneDayHour: getProfitForTimeRange(post, 'priceInComommonTokenOneDayLater'),
+      profitForSevenDays: getProfitForTimeRange(post, 'priceInComommonTokenOneWeekLater'),
+    }));
+
+  const preProcessedData = socialCalls ? processData(socialCalls) : [];
+  const sortedData = sortData<SocialPost, keyof SocialPost>(preProcessedData ?? [], sortConfig);
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
@@ -65,7 +92,12 @@ export default function SocialCalls() {
 
   return (
     <>
-      <SocialCallsTable socialCalls={socialCalls} loading={loading} />
+      <SocialCallsTable
+        socialCalls={sortedData}
+        loading={loading}
+        handleSortChange={handleSortChange}
+        sortConfig={sortConfig}
+      />
       <PaginationControls
         currentPage={currentPage}
         totalPages={totalPages}
