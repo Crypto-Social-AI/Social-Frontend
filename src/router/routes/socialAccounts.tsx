601edfa7d782cb506893cb0478d43f1cda8a0fb7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-01-13 18:42:42 +0000

    use custom hook for abstracted logic and remove repeated code

--- src/router/routes/socialAccounts.tsx
+++ src/router/routes/socialAccounts.tsx
@@ -1,75 +1,24 @@
-import { useEffect, useState } from 'react';
-import { useAddress } from '@thirdweb-dev/react';
 import Container from 'components/Container/Container';
 import SocialAccountsTable from 'components/SocialAccountsTable/SocialAccountsTable';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
-import { type SortConfig, type SocialAccountWithPost } from 'lib/types';
+import { type SocialAccountWithPost } from 'lib/types';
 import { getSocialAccounts } from 'lib/utils/requests/getSocialAccounts';
-import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
 import sortData from 'lib/utils/helpers/sorting/sorting';
+import useTableData from 'hooks/useTableData';
 
-// TODO: Add custom hook or HOC to keep code DRY (SocialCalls and SocialAccounts virtually the same code)
 export default function SocialAccounts() {
-  const address = useAddress();
-  const [socialAccounts, setSocialAccounts] = useState<SocialAccountWithPost[] | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [sortConfig, setSortConfig] = useState<SortConfig<keyof SocialAccountWithPost> | null>(null);
-  const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
-  const [loading, setLoading] = useState(false);
+  const processData = (data: SocialAccountWithPost[]) =>
+    data?.map((account) => ({
+      ...account,
+      postCount: account?.posts?.length || 0,
+    }));
 
-  const handleSortChange = (key: keyof SocialAccountWithPost) => {
-    setSortConfig((currentSortConfig): SortConfig<keyof SocialAccountWithPost> | null => {
-      if (currentSortConfig && currentSortConfig.key === key) {
-        return {
-          key,
-          direction: currentSortConfig.direction === 'asc' ? 'desc' : 'asc',
-        };
-      }
-      return { key, direction: 'asc' };
-    });
-  };
-
-  const socialAccountsWithPostsCount = socialAccounts?.map((account) => ({
-    ...account,
-    postCount: account?.posts?.length || 0,
-  }));
-  const sortedData = sortData<SocialAccountWithPost, keyof SocialAccountWithPost>(
-    socialAccountsWithPostsCount ?? [],
-    sortConfig,
+  const { data, loading, sortConfig, handlePageChange, handleSortChange, currentPage, totalPages } = useTableData(
+    getSocialAccounts,
+    processData,
   );
-
-  const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage);
-  };
-
-  const handleItemsPerPageChange = (newLimit: number) => {
-    setItemsPerPage(newLimit);
-    setCurrentPage(1);
-  };
-
-  const fetchData = async (page: number, limit: number) => {
-    if (address) {
-      setLoading(true);
-      try {
-        const data = await getSocialAccounts(page, limit);
-        setSocialAccounts(data.socialAccounts);
-        setTotalPages(data.totalPages);
-      } catch (error) {
-        console.error({ error });
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    async function fetchDataAsync() {
-      await fetchData(currentPage, itemsPerPage);
-    }
-
-    fetchDataAsync().catch(console.error);
-  }, [address, currentPage, itemsPerPage]);
+  const processedData = processData(data ?? []);
+  const sortedData = sortData<SocialAccountWithPost, keyof SocialAccountWithPost>(processedData ?? []);
 
   return (
     <Container className='px-12'>
@@ -83,7 +32,7 @@ export default function SocialAccounts() {
         currentPage={currentPage}
         totalPages={totalPages}
         onPageChange={handlePageChange}
-        onItemsPerPageChange={handleItemsPerPageChange}
+        onItemsPerPageChange={handlePageChange}
       />
     </Container>
   );
--- src/router/routes/socialCalls.tsx
+++ src/router/routes/socialCalls.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useAddress } from '@thirdweb-dev/react';
-import { type SocialCallsResponse, getSocialCalls } from 'lib/utils/requests/getSocialCalls';
-import { type SortConfig, type SocialPosts, type SocialPost } from 'lib/types';
-import { TABLE_RECORDS_PER_PAGE_LIMIT } from 'lib/utils/constants/general';
+import { getSocialCalls } from 'lib/utils/requests/getSocialCalls';
+import { type SocialPost } from 'lib/types';
 import sortData from 'lib/utils/helpers/sorting/sorting';
 import SocialCallsTable from 'components/SocialCallsTable/SocialCallsTable';
 import PaginationControls from 'components/PaginationControls/PaginationControls';
@@ -11,6 +8,7 @@ import {
   getMaxProfitPercentage,
   getProfitForTimeRange,
 } from 'lib/utils/helpers/calculations/calculateProfit';
+import useTableData from 'hooks/useTableData';
 
 type ExtendedSocialPost = SocialPost & {
   currentProfitLoss?: number | null;
@@ -21,28 +19,7 @@ type ExtendedSocialPost = SocialPost & {
   profitForSevenDays?: number | null;
 };
 
-// TODO: Add custom hook or HOC to keep code DRY (SocialCalls and SocialAccounts virtually the same code)
 export default function SocialCalls() {
-  const address = useAddress();
-  const [socialCalls, setSocialCalls] = useState<SocialPosts | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [sortConfig, setSortConfig] = useState<SortConfig<keyof SocialPost> | null>(null);
-  const [itemsPerPage, setItemsPerPage] = useState(TABLE_RECORDS_PER_PAGE_LIMIT);
-  const [loading, setLoading] = useState(false);
-
-  const handleSortChange = (key: keyof SocialPost) => {
-    setSortConfig((currentSortConfig): SortConfig<keyof SocialPost> | null => {
-      if (currentSortConfig && currentSortConfig.key === key) {
-        return {
-          key,
-          direction: currentSortConfig.direction === 'asc' ? 'desc' : 'asc',
-        };
-      }
-      return { key, direction: 'asc' };
-    });
-  };
-
   // consider wrapping in useMemo
   const processData = (data: SocialPost[]): ExtendedSocialPost[] =>
     data?.map((post) => ({
@@ -55,40 +32,19 @@ export default function SocialCalls() {
       profitForSevenDays: getProfitForTimeRange(post, 'priceInComommonTokenOneWeekLater'),
     }));
 
+  const {
+    data: socialCalls,
+    loading,
+    sortConfig,
+    currentPage,
+    totalPages,
+    handlePageChange,
+    handleItemsPerPageChange,
+    handleSortChange,
+  } = useTableData(getSocialCalls, processData);
+
   const preProcessedData = socialCalls ? processData(socialCalls) : [];
   const sortedData = sortData<SocialPost, keyof SocialPost>(preProcessedData ?? [], sortConfig);
-
-  const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage);
-  };
-
-  const handleItemsPerPageChange = (newLimit: number) => {
-    setItemsPerPage(newLimit);
-    setCurrentPage(1);
-  };
-
-  const fetchData = async (page: number, limit: number) => {
-    if (address) {
-      setLoading(true);
-      try {
-        const data: SocialCallsResponse = await getSocialCalls(page, limit);
-        setSocialCalls(data.socialPosts);
-        setTotalPages(data.totalPages);
-      } catch (error) {
-        console.error({ error });
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    async function fetchDataAsync() {
-      await fetchData(currentPage, itemsPerPage);
-    }
-
-    fetchDataAsync().catch(console.error);
-  }, [address, currentPage, itemsPerPage]);
 
   return (
     <>
