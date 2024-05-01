7a79b93d9f20b2d9323c98a239ee0f2248cb9d85
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-01 18:25:30 +0100

    update app logo (will probably be changed again)

--- src/components/ConnectWalletBox/ConnectWalletBox.tsx
+++ src/components/ConnectWalletBox/ConnectWalletBox.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import { ConnectWallet } from '@thirdweb-dev/react';
-import appLogo from 'assets/images/hype-hunter.png';
+import appLogo from 'assets/images/social-logo.png';
 
 export default function ConnectWalletBox() {
   return (
     <div className='flex justify-between items-center border-b border-b-slate-800 py-4 px-8'>
       <Link to='/'>
-        <div className='w-20 h-20'>
-          <img className='rounded-full' src={appLogo} alt='SocialAi' />
+        <div className='flex items-center gap-2'>
+          <div className='w-16 h-16'>
+            <img className='rounded-full' src={appLogo} alt='SocialAi' />
+          </div>
+          <span className='text-3xl'>SocialAi</span>
         </div>
       </Link>
       <ConnectWallet />
