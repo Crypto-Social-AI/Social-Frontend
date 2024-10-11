4cd2a11c70f108c3357784cd60d7e4363caf98be
Adam <adamyoungy678@gmail.com>
2024-10-11 20:32:14 +0100

    refactor: Refactor root component to improve wallet connection UI

--- src/router/routes/root.tsx
+++ src/router/routes/root.tsx
@@ -7,23 +7,27 @@ export default function Root() {
   const address = useAddress();
 
   return (
-    <Container className='px-12'>
-      {address ? (
-        <SocialCalls />
-      ) : (
-        <div className='flex flex-col gap-6 justify-center items-center mx-8 border border-yellow-400 rounded-xl py-6 px-4 bg-gradient-to-tr from-yellow-400 via-black to-black'>
-          <>
-            <div className='rounded-full w-16 h-16 bg-yellow-600 flex justify-center items-center'>
-              <FaWallet size={24} />
-            </div>
-            <h2 className='text-3xl text-contrast'>Connect wallet</h2>
-            <span className='text-text-primary'>Connect your wallet by clicking the button below to proceed</span>
-            <div>
-              <ConnectWallet />
-            </div>
-          </>
-        </div>
-      )}
-    </Container>
+    <div className='flex justify-center items-center min-h-screen'>
+      <Container className='px-12'>
+        {address ? (
+          <SocialCalls />
+        ) : (
+          <div className='max-w-[60rem] flex flex-col gap-6 justify-center items-center mx-8 border border-yellow-400 rounded-xl py-6 px-4 bg-gradient-to-tr from-yellow-400 via-black to-black'>
+            <>
+              <div className='rounded-full w-24 h-24 bg-yellow-600 flex justify-center items-center'>
+                <FaWallet size={36} />
+              </div>
+              <h2 className='text-5xl md:text-6xl lg:text-7xl text-contrast mb-2'>Connect wallet</h2>
+              <span className='text-xl md:text-2xl lg:text-2xl text-text-primary mb-6'>
+                Connect your wallet by clicking the button below to proceed
+              </span>
+              <div>
+                <ConnectWallet />
+              </div>
+            </>
+          </div>
+        )}
+      </Container>
+    </div>
   );
 }
