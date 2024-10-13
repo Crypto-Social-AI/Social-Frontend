54f1294f876c7fb8e4e791a8e8e3dc2d2fa30b93
Adam <adamyoungy678@gmail.com>
2024-10-13 01:28:47 +0100

    refactor: Add ExpandedContentFromSocialCalls component to SocialAccountsTable/ExpandedContent.tsx

--- src/components/SocialAccountsTable/ExpandedContent.tsx
+++ src/components/SocialAccountsTable/ExpandedContent.tsx
@@ -1,8 +1,9 @@
-import { formatDistanceToNowStrict } from 'date-fns';
+import ExpandedContentFromSocialCalls from 'components/SocialCallsTable/ExpandedContent/ExpandedContent';
 import socialCallsData from 'components/SocialCallsTable/socialCallsData';
 import Table from 'components/Table/Table';
-import { getFilteredColumnsData } from 'lib/utils/helpers/format/format';
+import { formatDistanceToNowStrict } from 'date-fns';
 import { type SocialPosts } from 'lib/types';
+import { getFilteredColumnsData } from 'lib/utils/helpers/format/format';
 
 type ExpandedContentProps = {
   posts: SocialPosts;
@@ -38,6 +39,8 @@ function ExpandedContent({ posts, loading }: ExpandedContentProps) {
       dataSrc={posts}
       loading={loading}
       idProp='_id'
+      isExpandable
+      renderExpandedContent={(record) => <ExpandedContentFromSocialCalls record={record} />}
     />
   );
 }
